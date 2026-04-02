8d6f3d8e198c1c3a57d5722ba55abd61edf56afe
Logan Reuter <loganreuter@Mac.home.local>
2026-04-01 20:21:20 -0500

    fix hover.js module format

--- api/hover.js
+++ api/hover.js
@@ -97,7 +97,7 @@ async function hoverFetch(path, token) {
   return await r.json();
 }
 
-export default async function handler(req, res) {
+module.exports = async function(req, res) {
   // CORS
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
